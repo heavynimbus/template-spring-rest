b9442b8b91808a5b8e1e7653dc4d657f677dec54
heavynimbus <hugo.wieder@gmail.com>
2023-03-03 15:42:45 +0100

    🐛 🔧 clean configuration

--- src/test/java/heavynimbus/templatespringrest/CucumberIntegrationTest.java
+++ src/test/java/heavynimbus/templatespringrest/CucumberIntegrationTest.java
@@ -5,8 +5,12 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features"}, glue = {
-    "com.junia.jeeproject.stepDefs",
-    "com.junia.jeeproject.config"
-}, plugin = {"pretty", "html:target/cucumber-reports.html"})
-public class CucumberIntegrationTest {}
+@CucumberOptions(
+    features = "/features",
+    glue = {
+        "heavynimbus.templatespringrest.stepDefs",
+        "heavynimbus.templatespringrest.config"
+    }, plugin = {"pretty", "html:target/cucumber-reports.html"})
+public class CucumberIntegrationTest {
+
+}
